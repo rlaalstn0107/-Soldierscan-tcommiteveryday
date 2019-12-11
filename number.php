7435dad126d2d62fd63b6c53dfab5f5f58926470
김민수 <minsu980107@naver.com>
2019-12-11 22:27:22 +0900

    1211 I learn php string
    1211 I learn php string

---
+++ number.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Number & Operator</h1>
+  <h2>1+1</h2>
+<?php
+print(1);
+ ?>
+ <h1>2-1</h1>
+ <?php
+ echo 2-1;
+  ?>
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<body>
+<?php
+echo "Hello \"w\"ord";
+ ?>
+ <h2>concatenation operator </h2>
+ <?php
+ echo "minsu"." is"." go up"
+  ?>
+  <h2>String length function</h2>
+  <?php
+  echo strlen("Hello world");
+
+  ?>
+
+</body>
+</html>
