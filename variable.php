14b94d4a0bf099fe05022297aeb6094ef9206183
김민수 <minsu980107@naver.com>
2019-12-13 23:02:37 +0900

    12-13  php function
    12-13  php function

---
+++ variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name="minsu variableVVVVV";
+    echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum ".$name."dolore eu fugia".$name." nulla p".$name."ariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui ".$name."u officia minsudeserunt mollit anim id est laborum.";
+    ?>
+    <?php
+    $a=1;
+    echo $a+1;
+     ?>
+
+  </body>
+</html>
