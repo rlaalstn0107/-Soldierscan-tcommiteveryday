f8d7baea57d18b2d019a3c20477cb4d8b857f522
김민수 <minsu980107@naver.com>
2019-12-17 23:23:07 +0900

    12_17unlucky day
    angry

---
+++ comparison.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Comparison Operators&amp;
+    Boolean data type </h1>
+  </body>
+  <?php
+  var_dump(1==1);
+  var_dump(2+14);
+  var_dump(1.523);
+   ?>
+</html>
